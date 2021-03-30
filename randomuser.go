528d3118500b1603184bf419580a6b0a6ad67dfb
Ernesto Alejandro Nappi <eanappi@gmail.com>
2021-03-30 18:03:24 -0300

    Function to request data JSON information

--- randomuser.go
+++ randomuser.go
@@ -1,6 +1,95 @@
 package randomuser
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+const endpoint = "https://randomuser.me/api/1.3/?nat=es&results=10&noinfo"
+
+// RandomuserScheme is the all randomuser scheme response
+type RandomuserScheme struct {
+	Results []struct {
+		Gender string `json:"gender"`
+		Name   struct {
+			Title string `json:"title"`
+			First string `json:"first"`
+			Last  string `json:"last"`
+		} `json:"name"`
+		Location struct {
+			Street struct {
+				Number int    `json:"number"`
+				Name   string `json:"name"`
+			} `json:"street"`
+			City        string `json:"city"`
+			State       string `json:"state"`
+			Country     string `json:"country"`
+			Postcode    int    `json:"postcode"`
+			Coordinates struct {
+				Latitude  string `json:"latitude"`
+				Longitude string `json:"longitude"`
+			} `json:"coordinates"`
+			Timezone struct {
+				Offset      string `json:"offset"`
+				Description string `json:"description"`
+			} `json:"timezone"`
+		} `json:"location"`
+		Email string `json:"email"`
+		Login struct {
+			UUID     string `json:"uuid"`
+			Username string `json:"username"`
+			Password string `json:"password"`
+			Salt     string `json:"salt"`
+			Md5      string `json:"md5"`
+			Sha1     string `json:"sha1"`
+			Sha256   string `json:"sha256"`
+		} `json:"login"`
+		Dob struct {
+			Date time.Time `json:"date"`
+			Age  int       `json:"age"`
+		} `json:"dob"`
+		Registered struct {
+			Date time.Time `json:"date"`
+			Age  int       `json:"age"`
+		} `json:"registered"`
+		Phone string `json:"phone"`
+		Cell  string `json:"cell"`
+		ID    struct {
+			Name  string `json:"name"`
+			Value string `json:"value"`
+		} `json:"id"`
+		Picture struct {
+			Large     string `json:"large"`
+			Medium    string `json:"medium"`
+			Thumbnail string `json:"thumbnail"`
+		} `json:"picture"`
+		Nat string `json:"nat"`
+	} `json:"results"`
+}
+
+func NewRandomUserJson() (*RandomuserScheme, error) {
+	var r RandomuserScheme
+
+	res, err := http.Get(endpoint)
+	defer res.Body.Close()
+
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Error not 200 OK: %s", res.StatusCode)
+	}
+
+	err = json.NewDecoder(res.Body).Decode(&r)
+
+	if err != nil {
+		return nil, fmt.Errorf("JSON type error: %s", err)
+	}
+
+	return &r, nil
+}
 
 func Test() {
 	fmt.Println("Modulo randomuser saludando")
